88594b68e8b775fca5e44ac86cb7b503ccc7715c
Johnny01232003 <89808275+Johnny01232003@users.noreply.github.com>
2021-12-04 02:43:05 -0800

    Moving pieces PT 2
    Started on the code for moving pieces.

--- main.py
+++ main.py
@@ -30,6 +30,31 @@
 # gameplay loop
 #     start on white
 #     
+    def makeMove(self, move):
+        self.board[move.startRow][
+class Move():
+    # maps keys to values
+    # key : value
+    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4,
+                   "5": 3, "6": 2, "7": 1, "8": 0}
+    rowsToRanks = {v: k for k, v in ranksToRows.items()}
+    filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3,
+                   "e": 4, "f": 5, "g": 6, "h": 7}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
+    
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+    def getChessNotation(self):
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
+        
+    def getRankFile(self, r, c):
+        return self.colsToFiles[c] + self.rowsToRanks[r]
+        
 #     if king is in check:
 #         restrict valid moves to whatever gets king out of check
 #     else:
